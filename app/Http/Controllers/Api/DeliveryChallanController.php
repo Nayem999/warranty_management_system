29fbc978911878219d2ca6de3a273c87738829a5
nayem999 <shalehahmed34@gmail.com>
2026-06-01 13:09:39 +0600

    status update

--- app/Http/Controllers/Api/DeliveryChallanController.php
+++ app/Http/Controllers/Api/DeliveryChallanController.php
@@ -87,7 +87,7 @@ class DeliveryChallanController extends Controller
                     'is_delivered'    => 1,
                     'delivery_id'     => $challan->id,
                     // 'status'          => "Delivered",
-                    'status_comment'  => $challan->delivered_remarks,
+                    // 'status_comment'  => $challan->delivered_remarks,
                     'wo_delivery_date'  =>  Carbon::now()->format('Y-m-d H:i:s'),
                 ]);
 
